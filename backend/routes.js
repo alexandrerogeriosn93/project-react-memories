1e74d475ed12db3ea46cebbf9c1c97765c8575d0
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-27 10:13:39 -0300

    feat: function to get memory by id

--- backend/routes.js
+++ backend/routes.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 const upload = require("./helpers/upload");
 
-const { createMemory, getMemories } = require("./controllers/MemoryController");
+const {
+  createMemory,
+  getMemories,
+  getMemory,
+} = require("./controllers/MemoryController");
 
 router.post(
   "/",
@@ -21,5 +25,7 @@ router.post(
 );
 
 router.get("/", (req, res) => getMemories(req, res));
+
+router.get("/:id", (req, res) => getMemory(req, res));
 
 module.exports = router;
