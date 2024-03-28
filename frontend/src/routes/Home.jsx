85fbec7ee75aa5e7934fe02e702818a004a5754f
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-28 09:24:06 -0300

    feat: add component Home styles css

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 
 import axios from "../axios-config";
 
+import "./Home.css";
+
 const Home = () => {
   const [memories, setMemories] = useState([]);
 
@@ -22,7 +24,18 @@ const Home = () => {
       <h2>Confira as últimas Memórias</h2>
       <div className="memories-container">
         {memories.length > 0 &&
-          memories.map((memory) => <p key={memory._id}>{memory.title}</p>)}
+          memories.map((memory) => (
+            <div className="memory" key={memory._id}>
+              <img
+                src={`${axios.defaults.baseURL}${memory.src}`}
+                alt={memory.title}
+              />
+              <p>{memory.title}</p>
+              <Link className="btn" to={`/memories/${memory._id}`}>
+                Comentar
+              </Link>
+            </div>
+          ))}
       </div>
     </div>
   );
