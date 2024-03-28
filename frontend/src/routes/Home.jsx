d5fa2913fc22bf224700fe606c5fc5fd6ffaf126
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-28 09:12:51 -0300

    feat: add function to get all Memories

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+
+import { Link } from "react-router-dom";
+
+import axios from "../axios-config";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [memories, setMemories] = useState([]);
+
+  useEffect(() => {
+    const getMemories = async () => {
+      const res = await axios.get("/memories");
+
+      setMemories(res.data);
+    };
+
+    getMemories();
+  }, []);
+
+  return (
+    <div className="home">
+      <h2>Confira as últimas Memórias</h2>
+      <div className="memories-container">
+        {memories.length > 0 &&
+          memories.map((memory) => <p key={memory._id}>{memory.title}</p>)}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
